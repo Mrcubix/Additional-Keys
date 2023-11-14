7512ca5b3a60a557cd2b8f2093f4b40b30d98320
Mrcubix <tintamare91@gmail.com>
2023-11-14 21:53:07 +0100

    Upodated build script ot be the same as in the 0.5.x branch

--- build.ps1
+++ build.ps1
@@ -1,2 +1,23 @@
-Remove-Item -Recurse -Force build/*
-dotnet publish -c Release -o build
+# Remove build folder & contents if it exists
+if (Test-Path -Path build) {
+    Remove-Item -Recurse -Force build/*
+}
+
+dotnet publish -c Release -o temp
+
+# create build folder if it doesn't exist
+if (!(Test-Path -Path .\build)) {
+    New-Item -ItemType Directory -Path .\build
+}
+
+# copy AdditionalKeys.dll to build folder
+Copy-Item .\temp\AdditionalKeys.dll .\build\AdditionalKeys.dll
+Copy-Item .\temp\AdditionalKeys.pdb .\build\AdditionalKeys.pdb
+
+# remove temp folder
+
+Remove-Item -Recurse -Force temp
+
+# zip the contents of the build folder
+
+Compress-Archive -Path .\build\* -DestinationPath ".\build\Additional-Keys-0.6.x-1.0.0.zip"
--- build.sh
+++ build.sh
@@ -1,2 +1,27 @@
-rm -rf build/*
-dotnet publish -c Release -o build
+#!/bin/bash
+
+# Remove old build files
+if [ -d "build" ]; then
+    rm -rf build
+fi
+
+dotnet publish -c Release -o temp
+
+# Create build directory
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+# Copy files to build directory
+cp temp/AdditionalKeys.dll build/AdditionalKeys.dll
+cp temp/AdditionalKeys.pdb build/AdditionalKeys.pdb
+
+# Remove temp directory
+rm -rf temp
+
+cd build
+
+# zip the contents of the build folder
+zip -r "Additional-Keys-0.6.x-1.0.0.zip" *
+
+cd ..
